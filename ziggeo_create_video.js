34d78747e83c05dfb53f5ee611635d64186eb582
flachdaniel <flachdaniel77@gmail.com>
2022-03-20 20:24:37 +0100

    create video submit form

--- ziggeo_create_video.js
+++ ziggeo_create_video.js
@@ -1,3 +1,13 @@
+var user = {};
+var record_id = "undefined";
+
+MemberStack.onReady.then(function (member) {
+    user.email = member["email"];
+    user.name = member["name"];
+    user.id = member["id"];
+    user.membershipTypeId = $memberstack.membership.status;
+});
+
 ziggeoApp.on("ready", function() {
     // Let's get the ziggeorecorder element reference
     var element = document.getElementById('myRecorder');
@@ -9,5 +19,50 @@ ziggeoApp.on("ready", function() {
     recorder.on("verified", function() {
       var videoToken = recorder.get('video'); // Video token is available after the event is triggered
       alert("The video with token " + videoToken + " has been submitted!");
+      record_id = videoToken;
     });
 });
+
+async function post_airtable_data(videoTitle, inputLanguage, outputLanguage, record_id) {
+  let result;
+  var data = {
+    memberId: user.id,
+    memberEmail: user.email,
+    videoTitle: videoTitle,
+    inputLanguage: inputLanguage,
+    outputLanguage: outputLanguage,
+    record_id: record_id
+  }
+  console.log("data: " + data)
+  try {
+      result = await $.ajax({
+          url: "https://hook.integromat.com/" + "y3h5to955pvfivo3faxg7gnhqw5x1td0",
+          type: 'POST',
+          data: data
+      });
+      console.log("Data successfully sent: ");
+      return result;
+  } catch (error) {
+      console.log("Error while getting data from integromat: ");
+      console.error(error);
+  }
+}
+
+$("#vid-form-submit").on("click", function () {
+  videoTitle = $("#Video-Title").val();
+  console.log(videoTitle);
+
+  inputLanguage = $("#Input-Language").val();
+  console.log(inputLanguage);
+
+  outputLanguage = $("#Output-Language").val();
+  console.log(outputLanguage);
+
+  if (record_id == "undefined") {
+    alert("Please record or upload a video!");
+  } else {
+    post_airtable_data(videoTitle, inputLanguage, outputLanguage, record_id);
+  }
+
+});
+

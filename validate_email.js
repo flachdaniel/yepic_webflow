09f42b2d854b5f1db230abfcf037395905b57ee6
flachdaniel <flachdaniel77@gmail.com>
2022-03-28 18:53:28 +0200

    github pages deployment error

--- validate_email.js
+++ validate_email.js
@@ -6,6 +6,21 @@ MemberStack.onReady.then(function (member) {
     user.membershipTypeId = $memberstack.membership.status;
 });
 
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+}
 
 async function send_data(custom_id) {
   let result;

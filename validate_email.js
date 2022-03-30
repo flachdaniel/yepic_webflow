eb3357b0cfa2bdaeefec0a5c8f7ae38d416ac43c
flachdaniel <flachdaniel77@gmail.com>
2022-03-30 20:04:33 +0200

    jquery onload

--- validate_email.js
+++ validate_email.js
@@ -6,46 +6,51 @@ MemberStack.onReady.then(function (member) {
     user.membershipTypeId = $memberstack.membership.status;
 });
 
-function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+jQuery(document).ready(function($){
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+    function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+    
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+    
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
         }
+        return false;
     }
-    return false;
-}
+    
+    function send_data(custom_id) {
+      let result;
+      var data = {
+        custom_id: custom_id
+      }
+      console.log("user id: " + user.id)
+      try {
+          result = $.ajax({
+              url: "https://hook.integromat.com/" + "51lp3k11p566j8q3s7a8v22q4tx29the",
+              type: 'POST',
+              data: data
+          });
+          console.log("Data successfully posted");
+          return result;
+      } catch (error) {
+          console.log("Error while getting posting data to integromat: ");
+          console.error(error);
+      }
+    }
+    
+    function verify_email(){
+        var custom_id = getUrlParameter('id');
+        send_data(custom_id);
+        console.log("Email is verified");
+    }
+    
+    setTimeout(verify_email, 1000);
+    
+});
 
-function send_data(custom_id) {
-  let result;
-  var data = {
-    custom_id: custom_id
-  }
-  console.log("user id: " + user.id)
-  try {
-      result = $.ajax({
-          url: "https://hook.integromat.com/" + "51lp3k11p566j8q3s7a8v22q4tx29the",
-          type: 'POST',
-          data: data
-      });
-      console.log("Data successfully posted");
-      return result;
-  } catch (error) {
-      console.log("Error while getting posting data to integromat: ");
-      console.error(error);
-  }
-}
-
-function verify_email(){
-    var custom_id = getUrlParameter('id');
-    send_data(custom_id);
-    console.log("Email is verified");
-}
-
-setTimeout(verify_email, 1000);

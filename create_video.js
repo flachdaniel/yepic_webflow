51b09dc7d87d180ccc3e29141b94ce2fb537c784
flachdaniel <flachdaniel77@gmail.com>
2022-03-30 23:10:12 +0200

    test

--- create_video.js
+++ create_video.js
@@ -317,12 +317,9 @@ async function isEmailVerified(id) {
 
 async function InitializeIsUserVerified() {
   const response = await isEmailVerified(fV.id);
-  console.log(response);
   const is_email_verified_json = JSON.parse(response);
-  console.log(is_email_verified_json);
-  console.log(is_email_verified_json.is_email_verified);
   
-  if (true) {
+  if (is_email_verified_json.is_email_verified) {
     $(".form-create-button-denied-wrap").css("display", "block");
     $(".popup-email-verify-wrap").css("display", "block");
     $(".form-listen-denied-wrap").css("display", "block");
